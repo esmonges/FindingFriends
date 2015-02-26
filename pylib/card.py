a83a38c679736551a3a0247da57e5a43e746e3f0
TED <edward.smongeski@gmail.com>
2015-02-25 22:49:57 -0500

    Actually checking in card and card_test

--- pylib/card.py
+++ pylib/card.py
@@ -1,3 +1,66 @@
+class Card(object):
+    """Class representing an individual card"""
+    NUMBERS = (
+    	'ONE',
+    	'TWO',
+    	'THREE',
+    	'FOUR',
+    	'FIVE',
+    	'SIX',
+    	'SEVEN',
+    	'EIGHT',
+    	'NINE',
+    	'TEN',
+    	'JACK',
+    	'QUEEN',
+    	'KING',
+    	'ACE',
+    )
+    JOKERNUMBER = ('JOKER',)
+    JOKERSUITS = (
+    	'BIG',
+    	'SMALL',
+    )
+    SUITS = (
+    	'CLUB',
+    	'SPADE',
+    	'HEART',
+    	'DIAMOND',
+    )
 
+    def __init__(self, suit, number):
+        super(Card, self).__init__()
 
+        # Card should have a number and a suit
+        # If it is not a joker, it should be
+        # one of the standard suits. If it is a joker,
+        # it should be BIG or SMALL instead of one
+        # of the standard suits
+        assert (number in self.NUMBERS + self.JOKERNUMBER)
+        if (number == 'JOKER'):
+        	assert suit in self.JOKERSUITS
+        else :
+        	assert suit in self.SUITS
+
+        self.suit = suit
+        self.number = number
+
+    @classmethod
+    # Getter for NUMBERS constant
+    def getNumbers(cls):
+        return Card.NUMBERS
+
+    @classmethod
+    # Getter for JOKERSUITS constant
+    def getJokerSuits(cls):
+        return Card.JOKERSUITS
+
+    @classmethod
+    # Getter for SUITS constant
+    def getSuits(cls):
+        return Card.SUITS
+
+    @classmethod
+    def getJokerNumber(cls):
+        return Card.JOKERNUMBER
 
--- pylib/card_test.py
+++ pylib/card_test.py
@@ -1,3 +1,34 @@
+import unittest
+from card import Card
 
+class Card_Test(unittest.TestCase):
+    def setUp(self):
+        self.numbers = Card.getNumbers() + Card.getJokerNumber()
+        self.suits = Card.getSuits()
+        self.jokersuits = Card.getJokerSuits()
 
+    def test_allSuits(self):
+        for suit in self.suits:
+            for number in self.numbers:
+                # Special case for jokers
+                if (number == 'JOKER'):
+                    with self.assertRaises(AssertionError):
+                        Card(suit, number)
+                else:
+                    Card(suit, number)
+
+    def test_jokerSuits(self):
+        for suit in self.jokersuits:
+            Card(suit, 'JOKER')
+
+    def test_badSuit(self):
+        with self.assertRaises(AssertionError):
+            Card('LOLNO', 'ONE')
+
+    def test_badNumber(self):
+        with self.assertRaises(AssertionError):
+            Card('SPADE', 'TREEFIDDY')
+
+if __name__ == '__main__':
+    unittest.main()
 
