d3847afe50f4caecedf219812e4f5cf4f61ea3ba
Arjun-Kiran <ajkiran1@gmail.com>
2020-04-25 13:03:18 -0400

    unit test tox framework. setup

---
+++ test/unit/test_card.py
@@ -0,0 +1,10 @@
+
+import pytest
+from unittest.mock import patch, MagicMock
+
+from src.Game.Modules.Card import Card
+from src.Game.Modules import CardConstants
+
+
+def test_card_tc_1():
+    assert True is True
